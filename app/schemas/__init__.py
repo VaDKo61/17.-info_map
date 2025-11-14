014f13b882f9c7d80958905082550caa1b9dae2f
VaDKo61 <vadkoo@mail.ru>
2025-11-14 22:32:35 +0300

    add in init BuildingOrganizationsResponse

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,9 +1,10 @@
 from .activity import ActivityBase
-from .building import BuildingBase
+from .building import BuildingBase, BuildingOrganizationsResponse
 from .organization import OrganizationBase
 
 __all__ = [
     'ActivityBase',
     'BuildingBase',
     'OrganizationBase',
+    'BuildingOrganizationsResponse',
 ]
