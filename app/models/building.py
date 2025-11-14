b03757b07691ac974fb1c155cb278e360d9d3747
VaDKo61 <vadkoo@mail.ru>
2025-11-14 07:54:43 +0300

    edit address building - unique

--- app/models/building.py
+++ app/models/building.py
@@ -8,7 +8,7 @@ class Building(Base):
     __tablename__ = 'buildings'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    address: Mapped[str] = mapped_column(String(250), nullable=False)
+    address: Mapped[str] = mapped_column(String(250), nullable=False, unique=True)
     latitude: Mapped[float] = mapped_column(Float, nullable=False)
     longitude: Mapped[float] = mapped_column(Float, nullable=False)
 
