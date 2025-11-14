ce4e1d5c55c8d98aa144fae53f639a7919c7d981
VaDKo61 <vadkoo@mail.ru>
2025-11-14 15:44:14 +0300

    create init package schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,9 @@
+from .activity import ActivityBase
+from .building import BuildingBase
+from .organization import OrganizationBase
+
+__all__ = [
+    'ActivityBase',
+    'BuildingBase',
+    'OrganizationBase',
+]
