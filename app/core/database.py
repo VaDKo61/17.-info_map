e58a5cc05e1600b66aca913902aa4287b5b505d0
VaDKo61 <vadkoo@mail.ru>
2025-11-14 08:23:28 +0300

    edit import database.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-from config import settings
+from core.config import settings
 
 Base = declarative_base()
 
