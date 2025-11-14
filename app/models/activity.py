f19505f8ee06b2f1a2fddd3a0d4fb8b50cd6f0ee
VaDKo61 <vadkoo@mail.ru>
2025-11-14 08:03:05 +0300

    create table activity

---
+++ app/models/activity.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from core.database import Base
+
+
+class Activity(Base):
+    __tablename__ = 'activities'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(250), nullable=False, unique=True)
+
+    parent_id: Mapped[int] = mapped_column(ForeignKey('activities_id'), nullable=True)
+    parent: Mapped['Activity'] = relationship('Activity', remote_side=[id], back_populates='children')
+    children: Mapped[list['Activity']] = relationship('Activity', back_populates='parent')
