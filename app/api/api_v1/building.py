dcf0e7a733160c6ed612460ddd1655310ea98419
VaDKo61 <vadkoo@mail.ru>
2025-11-15 17:44:32 +0300

    create endpoint '/search_area'

---
+++ app/api/api_v1/building.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Query, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.database import db_helper
+from schemas import BuildingRead
+from services import BuildingService
+
+router = APIRouter(tags=['buildings'])
+
+
+@router.get(
+    '/search_area',
+    response_model=list[BuildingRead]
+)
+async def search_buildings(
+        lat: float | None = Query(None),
+        lng: float | None = Query(None),
+        radius: float | None = Query(None, description='в км'),
+        lat_min: float | None = Query(None),
+        lat_max: float | None = Query(None),
+        lng_min: float | None = Query(None),
+        lng_max: float | None = Query(None),
+        session: AsyncSession = Depends(db_helper.session_getter)
+):
+    return await BuildingService.get_buildings_from_area(
+        session=session,
+        lat=lat,
+        lng=lng,
+        radius=radius,
+        lat_min=lat_min,
+        lat_max=lat_max,
+        lng_min=lng_min,
+        lng_max=lng_max
+    )
