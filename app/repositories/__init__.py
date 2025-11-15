8e7a39e00c3152cbb6a4ab9bffa93a72ee684e22
VaDKo61 <vadkoo@mail.ru>
2025-11-15 22:18:00 +0300

    add ActivityRepository in init

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,7 +1,9 @@
 from .organization import OrganizationRepository
 from .building import BuildingRepository
+from .activity import ActivityRepository
 
 __all__ = [
     'OrganizationRepository',
     'BuildingRepository',
+    'ActivityRepository',
 ]
