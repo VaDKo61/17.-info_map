8f8741d4348612088322563a2cbe54cfaabbb52a
VaDKo61 <vadkoo@mail.ru>
2025-11-15 17:43:55 +0300

    create prefix Building

--- app/core/config.py
+++ app/core/config.py
@@ -10,6 +10,7 @@ class RunConfig(BaseModel):
 class ApiV1Prefix(BaseModel):
     prefix: str = '/v1'
     organizations: str = '/organizations'
+    buildings: str = '/buildings'
 
 
 class ApiPrefix(BaseModel):
