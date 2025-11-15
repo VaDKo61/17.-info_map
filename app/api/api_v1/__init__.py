74db7f842319aa67e12ee73751ee1eb873d48bf9
VaDKo61 <vadkoo@mail.ru>
2025-11-15 17:44:47 +0300

    add buildings_router

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -3,10 +3,16 @@ from fastapi import APIRouter
 from core.config import settings
 
 from .organization import router as organization_router
+from .building import router as buildings_router
 
 router = APIRouter(prefix=settings.api.v1.prefix)
 
 router.include_router(
     organization_router,
     prefix=settings.api.v1.organizations,
+)
+
+router.include_router(
+    buildings_router,
+    prefix=settings.api.v1.buildings
 )
