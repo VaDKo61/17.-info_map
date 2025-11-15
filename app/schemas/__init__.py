7efacda200131c76dbaec938a2ab9a87374330d9
VaDKo61 <vadkoo@mail.ru>
2025-11-15 17:44:12 +0300

    add BuildingRead in init

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,8 @@
 from .organization import BuildingOrganizationsResponse, ActivityOrganizationsResponse
+from .building import BuildingRead
 
 __all__ = [
     'BuildingOrganizationsResponse',
     'ActivityOrganizationsResponse',
+    'BuildingRead',
 ]
