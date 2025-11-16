0c6f7804561c5991d9cbee05acf22c0af0adea29
VaDKo61 <vadkoo@mail.ru>
2025-11-16 17:01:33 +0300

    edit queue env_file

--- app/core/config.py
+++ app/core/config.py
@@ -28,7 +28,7 @@ class DataBaseConfig(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=('.env', '.env_template'),
+        env_file=('.env_template', '.env'),
         case_sensitive=False,
         env_nested_delimiter='__',
         env_prefix='APP_CONFIG__'
